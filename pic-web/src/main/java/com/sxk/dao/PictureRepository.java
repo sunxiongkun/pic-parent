d1bfa4a5df63ce965df53f44626cfff9d43c3fc5
sunxiongkun <sunxiongkun2018@163.com>
2019-02-15 15:24:00 +0800

    spring data rest

--- pic-web/src/main/java/com/sxk/dao/PictureRepository.java
+++ pic-web/src/main/java/com/sxk/dao/PictureRepository.java
@@ -4,10 +4,12 @@ import com.sxk.entity.Picture;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  *
  */
+@RepositoryRestResource(collectionResourceRel = "pic", path = "pic")
 public interface PictureRepository extends JpaRepository<Picture, Integer> {
 
   Page<Picture> findByCategory(Integer category, Pageable pageable);
