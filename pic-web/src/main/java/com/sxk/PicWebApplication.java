9b598e1bfeebac9e2a2392c78d2f56425edd15e2
sunxiongkun <sunxiongkun2018@163.com>
2019-02-15 15:43:36 +0800

    spring boot admin client

--- pic-web/src/main/java/com/sxk/PicWebApplication.java
+++ pic-web/src/main/java/com/sxk/PicWebApplication.java
@@ -1,9 +1,11 @@
 package com.sxk;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class PicWebApplication {
 
   public static void main(String[] args) {
